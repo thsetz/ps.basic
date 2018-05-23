9aeddcb403e53920f5bf62aee705400104279535
dr.setz <old4sun>
2018-05-23 22:53:46 +0200

    fix typo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -125,7 +125,7 @@ html_theme_path = ['.']
 
 # Try to use the RTD Theme
 # RTD doc says, that: if you set the html_style variable in your conf.py, it should default to using the default theme.
-html_style=xx
+html_style="xx"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
