7c1d7f5f26254223270d83d4eaf676189e6f5580
drsetz <ps.basic@setz.de>
2019-09-05 11:50:42 +0200

    another way to install graphviz

--- setup.py
+++ setup.py
@@ -26,7 +26,8 @@ except IOError:
 
 import sys
 
-INSTALL_REQUIRES=[ 'setuptools', 'docopt' ,'pygraphviz'  ]
+#INSTALL_REQUIRES=[ 'setuptools', 'docopt' ,'pygraphviz'  ]
+INSTALL_REQUIRES=[ 'setuptools', 'docopt'   ]
 
 setup(name='ps.basic',
       version=VERSION + version_suffix,
