c18f6ed0978d69acb8673eb866b3f811f5f661a5
drsetz <ps.basic@setz.de>
2019-09-05 15:32:04 +0200

    add commit from Jenkinsfile and list the content of the dist dir + increas output of versoion_incr.py

--- version_incr.py
+++ version_incr.py
@@ -2,7 +2,7 @@ data = open("VERSION.txt").read()
 ver, rev, mino = data.split('.')
 
 new_version = ver + '.' + rev + '.' + str(int(mino)+1)  # result: '1.10'
-print(new_version)
+print("OLD VERSION WAS %s NEW_VERSION IS %s"%(data,new_version))
 fp = open("VERSION.txt","w")
 fp.write(new_version)
 fp.close()
